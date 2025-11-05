8f0a0061c88edf0b3d3a29228e647b682dacdae6
Dhairya <dhairyarana2265@gmail.com>
2025-11-05 13:23:15 -0700

    Add project files.

---
+++ Login.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace CarRentalSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            panel1 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label5 = new Label();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(30, 58, 138);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(guna2CirclePictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(363, 500);
+            panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(229, 231, 235);
+            label4.Location = new Point(94, 437);
+            label4.Name = "label4";
+            label4.Size = new Size(182, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Quality Guarantee";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(229, 231, 235);
+            label3.Location = new Point(104, 404);
+            label3.Name = "label3";
+            label3.Size = new Size(161, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Wonderful Price";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(229, 231, 235);
+            label2.Location = new Point(122, 369);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Every Brand";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(229, 231, 235);
+            label1.Location = new Point(104, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(172, 28);
+            label1.TabIndex = 2;
+            label1.Text = "Car Rental Palace";
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.Image = (Image)resources.GetObject("guna2CirclePictureBox1.Image");
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(24, 104);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges1;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(320, 262);
+            guna2CirclePictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2CirclePictureBox1.TabIndex = 1;
+            guna2CirclePictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(30, 58, 138);
+            label5.Location = new Point(566, 37);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 35);
+            label5.TabIndex = 3;
+            label5.Text = "LOGIN";
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.CustomizableEdges = customizableEdges2;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.FromArgb(30, 58, 138);
+            guna2Button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(525, 369);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            guna2Button1.Size = new Size(217, 43);
+            guna2Button1.TabIndex = 4;
+            guna2Button1.Text = "Login";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Calibri", 16.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(30, 58, 138);
+            label6.Location = new Point(599, 415);
+            label6.Name = "label6";
+            label6.Size = new Size(73, 35);
+            label6.TabIndex = 5;
+            label6.Text = "Clear";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.FromArgb(30, 58, 138);
+            label7.Location = new Point(393, 154);
+            label7.Name = "label7";
+            label7.Size = new Size(93, 35);
+            label7.TabIndex = 6;
+            label7.Text = "UserID";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.FromArgb(30, 58, 138);
+            label8.Location = new Point(393, 247);
+            label8.Name = "label8";
+            label8.Size = new Size(126, 35);
+            label8.TabIndex = 7;
+            label8.Text = "Password";
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.BorderColor = Color.FromArgb(30, 58, 138);
+            guna2TextBox1.BorderThickness = 3;
+            guna2TextBox1.CustomizableEdges = customizableEdges4;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 9F);
+            guna2TextBox1.ForeColor = Color.FromArgb(30, 58, 138);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Location = new Point(525, 140);
+            guna2TextBox1.Margin = new Padding(3, 4, 3, 4);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PlaceholderForeColor = Color.White;
+            guna2TextBox1.PlaceholderText = "";
+            guna2TextBox1.ScrollBars = ScrollBars.Vertical;
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            guna2TextBox1.Size = new Size(286, 49);
+            guna2TextBox1.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            guna2TextBox1.TabIndex = 8;
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.BorderColor = Color.FromArgb(30, 58, 138);
+            guna2TextBox2.BorderThickness = 3;
+            guna2TextBox2.CustomizableEdges = customizableEdges6;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Font = new Font("Segoe UI", 9F);
+            guna2TextBox2.ForeColor = Color.FromArgb(30, 58, 138);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Location = new Point(525, 233);
+            guna2TextBox2.Margin = new Padding(3, 4, 3, 4);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PlaceholderForeColor = Color.White;
+            guna2TextBox2.PlaceholderText = "";
+            guna2TextBox2.ScrollBars = ScrollBars.Vertical;
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            guna2TextBox2.Size = new Size(286, 49);
+            guna2TextBox2.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            guna2TextBox2.TabIndex = 9;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(874, 500);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(guna2Button1);
+            Controls.Add(label5);
+            Controls.Add(panel1);
+            Name = "Login";
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Label label1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+    }
+}
---
+++ Splash.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CarRentalSystem
+{
+    partial class Splash
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Mycar = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            Myprogress = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            Percentage = new Label();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)Mycar).BeginInit();
+            Myprogress.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Mycar
+            // 
+            Mycar.FillColor = Color.FromArgb(30, 58, 138);
+            Mycar.Image = (Image)resources.GetObject("Mycar.Image");
+            Mycar.ImageRotate = 0F;
+            Mycar.Location = new Point(35, 51);
+            Mycar.Name = "Mycar";
+            Mycar.ShadowDecoration.CustomizableEdges = customizableEdges1;
+            Mycar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            Mycar.Size = new Size(146, 112);
+            Mycar.SizeMode = PictureBoxSizeMode.Zoom;
+            Mycar.TabIndex = 0;
+            Mycar.TabStop = false;
+            // 
+            // Myprogress
+            // 
+            Myprogress.Controls.Add(Percentage);
+            Myprogress.Controls.Add(Mycar);
+            Myprogress.FillColor = Color.FromArgb(30, 58, 138);
+            Myprogress.FillThickness = 8;
+            Myprogress.Font = new Font("Segoe UI", 12F);
+            Myprogress.ForeColor = Color.White;
+            Myprogress.Location = new Point(351, 111);
+            Myprogress.Minimum = 0;
+            Myprogress.Name = "Myprogress";
+            Myprogress.ProgressColor = Color.White;
+            Myprogress.ProgressColor2 = Color.White;
+            Myprogress.ProgressThickness = 8;
+            Myprogress.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            Myprogress.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            Myprogress.Size = new Size(215, 215);
+            Myprogress.TabIndex = 1;
+            Myprogress.Text = "guna2CircleProgressBar1";
+            // 
+            // Percentage
+            // 
+            Percentage.AutoSize = true;
+            Percentage.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Percentage.ForeColor = Color.FromArgb(229, 231, 235);
+            Percentage.Location = new Point(95, 166);
+            Percentage.Name = "Percentage";
+            Percentage.Size = new Size(25, 24);
+            Percentage.TabIndex = 4;
+            Percentage.Text = "%";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(229, 231, 235);
+            label1.Location = new Point(258, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(402, 54);
+            label1.TabIndex = 2;
+            label1.Text = "CAR RENTAL SYSTEM";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(229, 231, 235);
+            label2.Location = new Point(377, 353);
+            label2.Name = "label2";
+            label2.Size = new Size(170, 24);
+            label2.TabIndex = 3;
+            label2.Text = "Rent. Ride. Repeat.";
+            // 
+            // Splash
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(30, 58, 138);
+            ClientSize = new Size(909, 418);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Myprogress);
+            Name = "Splash";
+            Text = "Form1";
+            Load += Splash_Load;
+            ((System.ComponentModel.ISupportInitialize)Mycar).EndInit();
+            Myprogress.ResumeLayout(false);
+            Myprogress.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CirclePictureBox Mycar;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar Myprogress;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label Percentage;
+    }
+}
